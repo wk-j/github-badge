a875e9c5f5b8dac30c590decb77b1f73e8e933d7
wk-j <somnuk.wk@gmail.com>
2019-03-23 13:58:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## GitHub Badge
+
+[![NuGet](https://img.shields.io/nuget/v/github-badge.svg)](https://www.nuget.org/packages/github-badge)
+[![GitHub release](https://img.shields.io/github/release/wk-j/github-badge.svg?style=social)](https://github.com/wk-j/github-badge/releases)
---
+++ src/GitHubBadge/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let options = argv |> List.ofArray
+    match options with
+    | [repo] ->
+        let template =
+            "[![GitHub release](https://img.shields.io/github/release/wk-j/{repo}.svg?style=social)](https://github.com/wk-j/{repo}/releases)"
+                .Replace("{repo}", repo)
+                .Replace("\n", "")
+                .Trim()
+        printfn "%s" template
+        0
+    | _ -> -1
