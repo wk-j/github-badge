a093e4e1d9b447d144f3cc5d070c717334bc7595
wk-j <somnuk.wk@gmail.com>
2019-03-23 14:17:32 +0700

    Update style

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Build Status](https://dev.azure.com/wk-j/github-badge/_apis/build/status/wk-j.github-badge?branchName=master)](https://dev.azure.com/wk-j/github-badge/_build/latest?definitionId=36&branchName=master)
 [![NuGet](https://img.shields.io/nuget/v/wk.GitHubBadge.svg)](https://www.nuget.org/packages/wk.GitHubBadge)
-[![GitHub release](https://img.shields.io/github/release/wk-j/github-badge.svg?style=social)](https://github.com/wk-j/github-badge/releases)
+[![GitHub release](https://img.shields.io/github/release/wk-j/github-badge.svg?style=flat-square)](https://github.com/wk-j/github-badge/releases)
--- src/GitHubBadge/Program.fs
+++ src/GitHubBadge/Program.fs
@@ -1,12 +1,16 @@
 ﻿open System
 
+"""
+![GitHub release](https://img.shields.io/github/release/wk-j/github-badge.svg?style=flat-square)
+""" |> ignore
+
 [<EntryPoint>]
 let main argv =
     let options = argv |> List.ofArray
     match options with
     | [repo] ->
         let template =
-            "[![GitHub release](https://img.shields.io/github/release/wk-j/{repo}.svg?style=social)](https://github.com/wk-j/{repo}/releases)"
+            "[![GitHub release](https://img.shields.io/github/release/wk-j/{repo}.svg?style=flat-square)](https://github.com/wk-j/{repo}/releases)"
                 .Replace("{repo}", repo)
                 .Replace("\n", "")
                 .Trim()
