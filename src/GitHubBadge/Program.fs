435ac8c7b77766deb39751a397ef9d71a221e516
wk-j <somnuk.wk@gmail.com>
2019-04-03 19:51:06 +0700

    Update Program.fs

--- src/GitHubBadge/Program.fs
+++ src/GitHubBadge/Program.fs
@@ -10,7 +10,7 @@ let main argv =
     match options with
     | [repo] ->
         let template =
-            "[![GitHub release](https://img.shields.io/github/release/wk-j/{repo}.svg?style=flat-square)](https://github.com/wk-j/{repo}/releases)"
+            "[![GitHub release](https://img.shields.io/github/release/wk-j/{repo}.svg)](https://github.com/wk-j/{repo}/releases)"
                 .Replace("{repo}", repo)
                 .Replace("\n", "")
                 .Trim()
